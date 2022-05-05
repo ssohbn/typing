ce1930314c7aff5d93975989ae87c414ff7a245a
marc :D <92878876+ssohbn@users.noreply.github.com>
2022-05-05 18:17:07 +0000

    a

--- src/main.rs
+++ src/main.rs
@@ -2,20 +2,26 @@ use std::io::{self, Write};
 
 fn main() {
 	println!("herlo");
-	
-	loop {
-		let mut buffer = String::new();
+	let text = get_text();
+	for word in text {
+		println!("quick! type the word: {}", word);
 		let input = process_input();
 		println!("you wrote: {}", input);
-		
-	}
+		if word.eq(&input) {
+			println!("you did good!");
+		}
+	}		
 }
 
-fn process_input() -> &str {
+fn process_input() -> String {
 	print!("> ");	
 	io::stdout().flush().unwrap();
 	let mut buffer = String::new();	
 	io::stdin().read_line(&mut buffer).unwrap();
-	let trimmed = &(buffer.trim());
+	let trimmed = buffer.trim().to_string();
 	trimmed
+}
+
+fn get_text() -> Vec<&'static str> {
+	vec!["dog", "frog", "god"]
 }
